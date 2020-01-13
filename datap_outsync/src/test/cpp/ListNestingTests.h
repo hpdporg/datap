83683ad567c5e54cd065642a9f6a66fd4ec29a08
doenn <none@none.com>
2020-01-13 17:43:42 -0500

    2010 passing tests

--- datap_outsync/src/test/cpp/ListNestingTests.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIST_NESTING_TESTS_H
-#define LIST_NESTING_TESTS_H
-
-#include "gtest\gtest.h"
-
-
-
-#endif
--- datap_outsync/src/test/cpp/ListTests.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIST_TESTS_H
-#define LIST_TESTS_H
-
-#include "gtest\gtest.h"
-
-
-
-#endif
